5446657fa3fd68043bcb9a898f21fc6619daef37
okolodk <grisha.zhivot@bk.ru>
2026-01-15 16:56:58 +0300

    логика продолжения

--- backend/program.cs
+++ backend/program.cs
@@ -8,3 +8,21 @@ Console.WriteLine($"Привет, {name}!")
 
 Console.WriteLine(DateTime.Now.DayOfWeek);
 
+Console.Write("Хотите продолжить? (y/n): ");
+string s = Console.ReadLine().Trim().ToLower();
+
+while (true){
+    Console.Write("Хотите продолжить? (y/n): ");
+    string s = Console.ReadLine().Trim().ToLower();
+    if (s == "y"){
+        Console.WriteLine("Продолжаем.");
+        break;
+    }
+    else if (s == "n"){
+        Console.WriteLine("До свидания!");
+        break;
+    }
+    else{
+        Console.WriteLine("Пожалуйста, введите 'y' или 'n'.");
+    }
+}
