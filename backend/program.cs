f48517e7e8556a765a741d79117fa79e7b9a01cc
okolodk <grisha.zhivot@bk.ru>
2026-01-15 16:50:12 +0300

    ввод имени and день недели

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Менят зовут Катаржин Г.М.");
+Console.WriteLine("Менят зовут Алексеев Г.С.");
 Console.WriteLine(DateTime.Now);
+
+string name = Console.ReadLine()
+Console.WriteLine($"Привет, {name}!")
+
+Console.WriteLine(DateTime.Now.DayOfWeek);
+
